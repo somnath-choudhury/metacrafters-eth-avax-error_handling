22f0c4f06e2b954b3a2f4f10800c1960a53ee885
somnath-choudhury <102171721+somnath-choudhury@users.noreply.github.com>
2023-06-26 23:11:47 +0530

    Update errorHandling.sol

--- errorHandling.sol
+++ errorHandling.sol
@@ -1,1 +1,34 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity 0.8.18;
+
+contract ErrorHandling{
+
+    uint public count=0;
+
+    function testRequire(uint amount) public{
+        require(amount > 1000,"Amount must be greater than 1000");
+        count++;
+    }
+
+    function testRevert(uint amount) public {
+        if(!(amount > 1000)){
+            revert("Amount must be greater than 1000");
+        }
+        count++;
+    }
+
+    function testAssert(uint amount) public{
+        assert(amount > 1000);
+        count++;
+    }
+
+    error CustomError(uint amount, string message);
+
+    function testCustomError(uint amount) public{
+        if(amount <= 1000){
+            revert CustomError(amount,"Amount must be greater than 1000");
+        }
+        count++;
+    }
+}
